7b033076c4d563edd364eb456e778a95f2ca7b1b
Shadow32055 <Shadow32055@gmail.com>
2023-03-21 17:01:05 -0400

    Update MissionCombatMechanicsHelperPatch.cs
    Fixed: Range damage not working

--- BetterAttributes/Patches/MissionCombatMechanicsHelperPatch.cs
+++ BetterAttributes/Patches/MissionCombatMechanicsHelperPatch.cs
@@ -3,6 +3,7 @@ using HarmonyLib;
 using System;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 
 namespace BetterAttributes.Patches {
@@ -17,18 +18,30 @@ namespace BetterAttributes.Patches {
                     return;
 
                 if (Helper.settings.melDmgBonusEnabled && attackerWeapon.IsMeleeWeapon) {
+
                     if (attackInformation.IsAttackerAIControlled && Helper.settings.melDmgBonusPlayerOnly)
                         return;
 
-                    inflictedDamage = inflictedDamage * (int)(Helper.GetAttributeEffect(Helper.settings.melDmgBonus, Helper.GetAttributeTypeFromText(Helper.settings.melDmgBonusAttribute), (CharacterObject)attackInformation.AttackerAgentCharacter) + 1);
-                } else if (Helper.settings.rngDmgBonusEnabled && attackerWeapon.IsRangedWeapon) {
-                    if (attackInformation.IsAttackerAIControlled && Helper.settings.rngDmgBonusPlayerOnly)
-                        return;
-
-                    inflictedDamage = inflictedDamage * (int)(Helper.GetAttributeEffect(Helper.settings.rngDmgBonus, Helper.GetAttributeTypeFromText(Helper.settings.rngDmgBonusAttribute), (CharacterObject)attackInformation.AttackerAgentCharacter) + 1);
+                    inflictedDamage = inflictedDamage * (int)( Helper.GetAttributeEffect(Helper.settings.melDmgBonus, Helper.GetAttributeTypeFromText(Helper.settings.melDmgBonusAttribute), (CharacterObject)attackInformation.AttackerAgentCharacter) + 1 );
                 }
             } catch (Exception e) {
                 Helper.WriteToLog("Issue with MissionCombatMechanicsHelperPatch.ComputeBlowDamage postfix. Exception output: " + e);
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MissionCombatMechanicsHelper), "ComputeBlowMagnitude")]
+        public static void ComputeBlowMagnitude(ref AttackCollisionData acd, ref AttackInformation attackInformation, MissionWeapon weapon, float momentumRemaining, bool cancelDamage, bool hitWithAnotherBone, Vec2 attackerVelocity, Vec2 victimVelocity, ref float baseMagnitude, ref float specialMagnitude, ref float movementSpeedDamageModifier, ref int speedBonusInt) {
+            if (!attackInformation.AttackerAgentCharacter.IsHero)
+                return;
+
+            if (Helper.settings.rngDmgBonusEnabled) {
+
+                if (attackInformation.IsAttackerAIControlled && Helper.settings.rngDmgBonusPlayerOnly)
+                    return;
+
+                float val = Helper.GetAttributeEffect(Helper.settings.rngDmgBonus, Helper.GetAttributeTypeFromText(Helper.settings.rngDmgBonusAttribute), (CharacterObject)attackInformation.AttackerAgentCharacter) + 1;
+                specialMagnitude *= val;
             }
         }
     }
